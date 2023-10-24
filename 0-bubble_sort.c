91facbcdb8edb4c6d8627591b7221776a27b1e75
EL kharmoudi Chadia <134792897+chadia08@users.noreply.github.com>
2023-10-24 01:21:42 +0100

    Create 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-/**
+/***
 * bubble_sort - a function that sorts an array of integers in
 * ascending order using the Bubble sort algorithm
 * @array: array of integers
